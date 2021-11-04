c1403ae71e348945b9bbfaecfa653f8af43ca39f
aleksander tikhonov <tihon2209@mail.ru>
2021-11-04 13:33:52 +0300

    Added Kafka

--- FoodMonitoring/configuration/src/main/java/com/senla/courses/configuration/FoodMonitoringConfiguration.java
+++ FoodMonitoring/configuration/src/main/java/com/senla/courses/configuration/FoodMonitoringConfiguration.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-@Import({DataSourceConfig.class})
+@Import({DataSourceConfig.class, KafkaTopicConfig.class, KafkaProducerConfig.class})
 @PropertySource({"classpath:jdbc.properties", "classpath:log4j2.properties" })
 @ComponentScan(basePackages = "com.senla.courses")
 public class FoodMonitoringConfiguration {
